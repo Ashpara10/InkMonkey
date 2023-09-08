2ec4af798b683b375ec52049e87526d0d977b911
AshPara10 <ashwinparande1156610c@gmail.com>
2023-09-08 23:46:57 +0530

    err res

--- my-app/app/api/note/route.ts
+++ my-app/app/api/note/route.ts
@@ -1,17 +1,17 @@
+import basepath from "@/lib/path";
 import { Note } from "@/lib/types";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const user = Number(req.cookies.get("user"));
-  const token = String(req.cookies.get("token"));
-  console.log({ user, token });
+  const token = req.cookies.get("token")?.value.toString() 
   const res = await fetch(
-    `http://localhost:8000/api/v1/note/${user}/getnotes`,
+    `${basepath}/api/v1/note/${user}/getnotes`,
     {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Auth-Token": token,
+        "Auth-Token": token as string,
       },
     }
   );
--- my-app/app/dashboard/note/[user]/[...id]/page.tsx
+++ my-app/app/dashboard/note/[user]/[...id]/page.tsx
@@ -1,32 +1,33 @@
-import NotePage from "@/components/note-page"
-import { Note } from "@/lib/types"
-import { cookies } from "next/headers"
+import NotePage from "@/components/note-page";
+import basepath from "@/lib/path";
+import { Note } from "@/lib/types";
+import { cookies } from "next/headers";
 
-const getNoteByID = async (user: string, id: string): Promise<{ status: boolean, note: Note | null }> => {
-    const token = cookies().get("token")?.value
+const getNoteByID = async (
+  user: string,
+  id: string
+): Promise<{ status: boolean; note: Note | null }> => {
+  const token = cookies().get("token")?.value;
 
-    const res = await fetch(`http://127.0.0.1:8000/api/v1/note/${user}/getnote/${id}`, {
-        headers: {
-            "Auth-Token": token as string
-        },
-        cache:"force-cache"
-    })
-    const data = await res.json()
-    if (!res.ok) return { status: false, note: null }
-    return { status: true, note: data?.data }
-}
+  const res = await fetch(`${basepath}/api/v1/note/${user}/getnote/${id}`, {
+    headers: {
+      "Auth-Token": token as string,
+    },
+    cache: "force-cache",
+  });
+  const data = await res.json();
+  if (!res.ok) return { status: false, note: null };
+  return { status: true, note: data?.data };
+};
 
+const Page = async ({ params }: { params: { user: string; id: string } }) => {
+  const { note, status } = await getNoteByID(params?.user, params?.id);
 
-const Page = async ({ params }: { params: { user: string, id: string } }) => {
+  return (
+    <div className="w-full min-h-screen p-4 flex flex-col items-center justify-center">
+      <NotePage data={note} status={status} />
+    </div>
+  );
+};
 
-    const { note, status } = await getNoteByID(params?.user, params?.id)
-
-    return (
-        <div className='w-full min-h-screen p-4 flex flex-col items-center justify-center'>
-
-            <NotePage data={note} status={status} />
-        </div>
-    )
-}
-
-export default Page
+export default Page;
